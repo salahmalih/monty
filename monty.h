a235c51addfc2b4f5f8038abee30f9e60b48f8ed
salahmalih <C130084552@taalim.ma>
2024-02-01 15:50:04 +0100

    test

--- monty.h
+++ monty.h
@@ -39,7 +39,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t *head = NULL;
+extern stack_t *head;
 typedef void (*op_func)(stack_t **, unsigned int);
 
 
